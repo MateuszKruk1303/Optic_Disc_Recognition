abd4e9d16daf68458c9cf1f7c72a46e36196ad6d
BorsukRomeo <milosz.homa@vp.pl>
2021-06-14 21:23:24 +0200

    Update Integrated.py

--- Integrated.py
+++ Integrated.py
@@ -1,12 +1,13 @@
-# img_viewer.py
+#!/usr/bin/env python
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
-from PIL import Image, ImageEnhance, ImageFilter
-
 import PySimpleGUI as sg
-import os.path
+import os
+from PIL import Image, ImageEnhance, ImageFilter, ImageTk
+import io
 
+filename="Resources/eye.jpg"
 
 # First the window layout in 2 columns
 
@@ -115,93 +116,123 @@ def CircleDetection(img_url, img, r_min, r_max):
     detectCircles(base_image_resized, edges, r_min, r_max)
 
 
-file_list_column = [
-    [
-        sg.Text("Image Folder"),
-        sg.In(size=(25, 1), enable_events=True, key="-FOLDER-"),
-        sg.FolderBrowse(),
-        sg.Button("Run!",enable_events=True,key="-RUN-"),
-        sg.InputText("promien minimalny",key="-IN1-"),
-        sg.InputText("przedzial w gore",key="-IN2-"),
-        
-             
+# Get the folder containin:g the images from the user
+folder = sg.popup_get_folder('Image folder to open', default_path='')
+if not folder:
+    sg.popup_cancel('Cancelling')
+    raise SystemExit()
 
-    ],
-    [
-        sg.Listbox(
-            values=[], enable_events=True, size=(40, 20), key="-FILE LIST-"
-        )
-    ],
-]
+# PIL supported image types
+img_types = (".png", ".jpg", "jpeg", ".tiff", ".bmp")
 
-# For now will only show the name of the file that was chosen
-image_viewer_column = [
-    [sg.Text("Choose an image from list on left:")],
-    [sg.Text(size=(40, 1), key="-TOUT-")],
-    [sg.Image(key="-IMAGE-")],
-]
+# get list of files in folder
+flist0 = os.listdir(folder)
 
-# ----- Full layout -----
-layout = [
-    [
-        sg.Column(file_list_column),
-        sg.VSeperator(),
-        sg.Column(image_viewer_column),
-       
-    ]
-]
+# create sub list of image files (no sub folders, no wrong file types)
+fnames = [f for f in flist0 if os.path.isfile(
+    os.path.join(folder, f)) and f.lower().endswith(img_types)]
 
-window = sg.Window("Image Viewer", layout)
+num_files = len(fnames)                # number of iamges found
+if num_files == 0:
+    sg.popup('No files in folder')
+    raise SystemExit()
 
-# Run the Event Loop
+del flist0                             # no longer needed
+
+# ------------------------------------------------------------------------------
+# use PIL to read data of one image
+# ------------------------------------------------------------------------------
+
+
+def get_img_data(f, maxsize=(1200, 850), first=False):
+    """Generate image data using PIL
+    """
+    img = Image.open(f)
+    img.thumbnail(maxsize)
+    if first:                     # tkinter is inactive the first time
+        bio = io.BytesIO()
+        img.save(bio, format="PNG")
+        del img
+        return bio.getvalue()
+    return ImageTk.PhotoImage(img)
+# ------------------------------------------------------------------------------
+
+
+# make these 2 elements outside the layout as we want to "update" them later
+# initialize to the first file in the list
+filename = os.path.join(folder, fnames[0])  # name of first file in list
+image_elem = sg.Image(data=get_img_data(filename, first=True))
+filename_display_elem = sg.Text(filename, size=(80, 3))
+file_num_display_elem = sg.Text('File 1 of {}'.format(num_files), size=(15, 1))
+
+# define layout, show and read the form
+col = [[filename_display_elem],
+       [image_elem]]
+
+col_files = [[sg.Listbox(values=fnames, change_submits=True, size=(60, 30), key='listbox')],
+             [sg.Button('Next', size=(8, 2)), sg.Button('Prev', size=(8, 2)), file_num_display_elem],
+             [sg.InputText("promien minimalny",key="-IN1-"),sg.InputText("przedzial w gore",key="-IN2-")],
+             [sg.Button('RUN!',size=(8,2)),sg.Text("enter values and run",key="output_text")]]
+
+layout = [[sg.Column(col_files), sg.Column(col)]]
+
+window = sg.Window('Image Browser', layout, return_keyboard_events=True,
+                   location=(0, 0), use_default_focus=False)
+
+# loop reading the user input and displaying image, filename
+i = 0
 while True:
+    # read the form
     event, values = window.read()
-    if event == "Exit" or event == sg.WIN_CLOSED:
+    print(event, values)
+    # perform button and keyboard operations
+    if event == sg.WIN_CLOSED:
         break
-    # Folder name was filled in, make a list of files in the folder
-    if event == "-FOLDER-":
-        folder = values["-FOLDER-"]
-        try:
-            # Get list of files in folder
-            file_list = os.listdir(folder)
-        except:
-            file_list = []
+    elif event in ('Next', 'MouseWheel:Down', 'Down:40', 'Next:34'):
+        i += 1
+        if i >= num_files:
+            i -= num_files
+        filename = os.path.join(folder, fnames[i])
+    elif event in ('RUN!'):
+        print(filename)
+        #run script with parameters-------------------------------------------------
+        img = Image.open(filename).convert("L")
+      
+        r_min=values["-IN1-"]
+        r_max=values["-IN2-"]
 
-        fnames = [
-            f
-            for f in file_list
-            if os.path.isfile(os.path.join(folder, f))
-            and f.lower().endswith((".png", ".jpg", ".gif"))
-        ]
-        window["-FILE LIST-"].update(fnames)
-    elif event == "-FILE LIST-":  # A file was chosen from the listbox
-        try:
-            filename = os.path.join(
-                values["-FOLDER-"], values["-FILE LIST-"][0]
-            )
-            print(filename)
-            print(filename.index("Resources"))
-            # filename = filename[filename.index("Resources"):len(filename):1]
-            #window["-TOUT-"].update(filename)
-            #window["-IMAGE-"].update(filename=filename)
-            imagen = filename
-        except Exception as ex:
-            print(ex)
-            pass
-    elif event == "-RUN-":
-        try:
-            print(imagen)
-            Image.open(imagen)
-            img = Image.open(imagen).convert("RGB")
-            print(values["-IN1-"])
-            print(values["-IN2-"])
-            r_min=int(values["-IN1-"])
-            r_max=int(values["-IN2-"])
-            CircleDetection(imagen, img, r_min, r_max)
-            print(r_min+r_max)
-        except:
-            pass
+        if((r_min.isnumeric()) and (r_max.isnumeric())):
+                r_min=int(values["-IN1-"])
+                r_max=int(values["-IN2-"])
+
+                
+                CircleDetection(filename, img, r_min, r_max)
+              
+
+                print(r_min)
+                print(r_max)
+        else:
+            print("values should be an integer")
         
-            
+        
+
+    elif event in ('Prev', 'MouseWheel:Up', 'Up:38', 'Prior:33'):
+        i -= 1
+        if i < 0:
+            i = num_files + i
+        filename = os.path.join(folder, fnames[i])
+    elif event == 'listbox':            # something from the listbox
+        f = values["listbox"][0]            # selected filename
+        filename = os.path.join(folder, f)  # read this file
+        i = fnames.index(f)                 # update running index
+    else:
+        filename = os.path.join(folder, fnames[i])
+
+    # update window with new image
+    image_elem.update(data=get_img_data(filename, first=True))
+    # update window with filename
+    filename_display_elem.update(filename)
+    # update page display
+    file_num_display_elem.update('File {} of {}'.format(i+1, num_files))
 
 window.close()
